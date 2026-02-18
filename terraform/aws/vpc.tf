a14c8d6590fdf2a080d608ec292f91d12e32e4e0
Dinesh Mohan Mekala <135266880+dm1121@users.noreply.github.com>
2026-02-18 17:57:34 -0600

    Add internet gateway and route table for public access

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -1,21 +1,25 @@
-resource "aws_vpc" "main" {
-  cidr_block           = "${var.cidr_prefix}.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name        = "${var.environment}-aws-vpc"
-    Environment = var.environment
+    Name = "${var.environment}-aws-igw"
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
-  availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "${var.environment}-aws-public-subnet"
+    Name = "${var.environment}-aws-public-rt"
   }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
