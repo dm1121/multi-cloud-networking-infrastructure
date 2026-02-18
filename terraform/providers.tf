73bbd371535c4f7e92611db1b455d96ad8c1c986
Dinesh Mohan Mekala <135266880+dm1121@users.noreply.github.com>
2026-02-18 17:51:13 -0600

    Add provider configurations for AWS, Azure, and Google

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+# terraform/providers.tf - Provider configurations
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.70"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
